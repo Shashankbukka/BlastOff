75b680bc5c0a72dc0adcebeec3c328e2bc046c6d
Shashank Bukka <shashankbukka@gmail.com>
2018-11-03 04:32:27 -0400

    Added Oscillator gameplay functionality for Obstacles, particle effects for the RocketShip.

---
+++ BlastOff/Assets/Oscillator.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[DisallowMultipleComponent]
+public class Oscillator : MonoBehaviour {
+
+    //Length of units to be moved in world space
+    [SerializeField] Vector3 movementVec = new Vector3(10,10,10);
+    [SerializeField] float period = 2f;
+
+    const float Pi = 3.14f;
+
+    //A movement Factor to control the swing in Range 0 to 1
+    [Range(0.01f,1)] [SerializeField] float movementFactor = 0.0f;
+
+    Vector3 startingPosition;
+
+	// Use this for initialization
+	void Start ()
+    {
+        startingPosition = transform.position;
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+      
+        float cycles = Time.time / period;
+        float fullSineWave = Mathf.Sin(cycles * Pi * 2);
+        movementFactor = (fullSineWave / 2) + 0.5f;
+        Vector3 offset = movementVec * movementFactor;
+        transform.position = startingPosition + offset;
+		
+	}
+}
--- BlastOff/Assets/Scripts/Rocket.cs
+++ BlastOff/Assets/Scripts/Rocket.cs
@@ -2,71 +2,133 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Rocket : MonoBehaviour {
 
-    Rigidbody rigidBody;
-    AudioSource thrustAudio;
+	Rigidbody rigidBody;
+	AudioSource audioSource;
 
-    [SerializeField] float rthrustVal = 250;
-    [SerializeField] float uthrustVal = 50;
+	[SerializeField] float rthrustVal = 250;
+	[SerializeField] float uthrustVal = 50;
+	[SerializeField] AudioClip thrustSound;
+	[SerializeField] AudioClip deathSound;
+	[SerializeField] AudioClip winSound;
 
-    // Use this for initialization
-    void Start () {
-        rigidBody = GetComponent<Rigidbody>();
-        thrustAudio = GetComponent<AudioSource>();
+	[SerializeField] ParticleSystem thrustEffects;
+	[SerializeField] ParticleSystem deathEffects;
+	[SerializeField] ParticleSystem winEffects;
+
+	enum State {Alive, Dead, Transcending };
+
+	State state;
+	// Use this for initialization
+	void Start () {
+		rigidBody = GetComponent<Rigidbody>();
+		audioSource = GetComponent<AudioSource>();
+		state = State.Alive;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        Thrust();
-        Rotate();
-    }
+		if (state == State.Alive)
+		{
+			RespondToThrustInput();
+			RespondToRotateInput();
+		}
+	}
 
 
-    private void OnCollisionEnter(Collision collision)
-    {
-        switch (collision.gameObject.tag)
+
+
+	private void OnCollisionEnter(Collision collision)
+	{
+		if (state != State.Alive)
+		{
+			return;
+		}
+
+		switch (collision.gameObject.tag)
+		{
+			//Friendly not being used anymore
+			case "Friendly":
+					break;
+			case "Finish":
+                state = State.Transcending;
+				audioSource.PlayOneShot(winSound);
+                winEffects.Play();
+				Invoke("LoadNextScene",1f);
+				break;
+			default:
+				state = State.Dead;
+                if(!deathEffects.isPlaying)
+				deathEffects.Play();
+				audioSource.PlayOneShot(deathSound);
+				Invoke("LoadFirstScene", 1f);
+				break;
+		}
+	}
+
+	//TODO: add additional level capability
+	private void LoadNextScene()
+	{
+
+		SceneManager.LoadScene(1);
+	}
+
+
+	private void LoadFirstScene()
+	{
+		SceneManager.LoadScene(0);
+	}
+
+   
+	//Vertical thrust movement with sound component
+	private void RespondToThrustInput()
+	{
+		if (Input.GetKey(KeyCode.Space))
+		{
+			ApplyThrust();
+
+		}
+		else if (audioSource.isPlaying)
+		{
+			audioSource.Stop();
+			thrustEffects.Stop();
+		}
+	}
+
+	private void ApplyThrust()
+	{
+		rigidBody.AddRelativeForce(Vector3.up * uthrustVal);
+		if (!audioSource.isPlaying)
+		{
+			audioSource.PlayOneShot(thrustSound);
+		}
+        if (!thrustEffects.isPlaying)
         {
-            case "Friendly":
-                    print("Friendly Object");
-                    break;
+            thrustEffects.Play();
         }
-    }
-    //Vertical thrust movement with sound component
-    private void Thrust()
-    {
-        if (Input.GetKey(KeyCode.Space))
-        {
-            rigidBody.AddRelativeForce(Vector3.up * uthrustVal);
-            if (!thrustAudio.isPlaying)
-            {
-                thrustAudio.Play();
-            }
-        }
-        else if (thrustAudio.isPlaying)
-        {
-            thrustAudio.Stop();
-        }
-    }
+	}
 
-    //Rotation of the Space Ship
-    private void Rotate()
-    {
-        rigidBody.freezeRotation = true;
 
-        float rotationThisFrame = rthrustVal * Time.deltaTime;
-        if (Input.GetKey(KeyCode.A))
-        {
-            transform.Rotate((Vector3.forward) * rotationThisFrame);
-        }
+	//Rotation of the Space Ship
+	private void RespondToRotateInput()
+	{
+		rigidBody.freezeRotation = true;
 
-        if (Input.GetKey(KeyCode.D))
-        {
-            transform.Rotate(-(Vector3.forward) * rotationThisFrame);
-        }
+		float rotationThisFrame = rthrustVal * Time.deltaTime;
+		if (Input.GetKey(KeyCode.A))
+		{
+			transform.Rotate((Vector3.forward) * rotationThisFrame);
+		}
 
-        rigidBody.freezeRotation = false;
-    }
+		if (Input.GetKey(KeyCode.D))
+		{
+			transform.Rotate(-(Vector3.forward) * rotationThisFrame);
+		}
+
+		rigidBody.freezeRotation = false;
+	}
 
 }
