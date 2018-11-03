47df08ccd8a4a88c43138b5fee2919f33003dcc0
Shashank Bukka <shashankbukka@gmail.com>
2018-11-02 22:10:49 -0400

    Major Push: Added new materials Finessed the spaceship movement Added thrust sound effect Created two levels with varying difficulty

--- BlastOff/Assets/Scripts/Rocket.cs
+++ BlastOff/Assets/Scripts/Rocket.cs
@@ -5,31 +5,68 @@ using UnityEngine;
 
 public class Rocket : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
+    Rigidbody rigidBody;
+    AudioSource thrustAudio;
+
+    [SerializeField] float rthrustVal = 250;
+    [SerializeField] float uthrustVal = 50;
+
+    // Use this for initialization
+    void Start () {
+        rigidBody = GetComponent<Rigidbody>();
+        thrustAudio = GetComponent<AudioSource>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        ProcessInput();
-	}
+        Thrust();
+        Rotate();
+    }
 
-    private void ProcessInput()
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        switch (collision.gameObject.tag)
+        {
+            case "Friendly":
+                    print("Friendly Object");
+                    break;
+        }
+    }
+    //Vertical thrust movement with sound component
+    private void Thrust()
     {
         if (Input.GetKey(KeyCode.Space))
         {
-            print("Space is Pressed");
+            rigidBody.AddRelativeForce(Vector3.up * uthrustVal);
+            if (!thrustAudio.isPlaying)
+            {
+                thrustAudio.Play();
+            }
         }
+        else if (thrustAudio.isPlaying)
+        {
+            thrustAudio.Stop();
+        }
+    }
 
+    //Rotation of the Space Ship
+    private void Rotate()
+    {
+        rigidBody.freezeRotation = true;
+
+        float rotationThisFrame = rthrustVal * Time.deltaTime;
         if (Input.GetKey(KeyCode.A))
         {
-            print("Rotating Left");
+            transform.Rotate((Vector3.forward) * rotationThisFrame);
         }
 
         if (Input.GetKey(KeyCode.D))
         {
-            print("Rotating Right");
+            transform.Rotate(-(Vector3.forward) * rotationThisFrame);
         }
+
+        rigidBody.freezeRotation = false;
     }
+
 }
